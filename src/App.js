7c999cf81e779f50d92760dec0e9bdcbd3fb3def
Tom McGinty <thmcginty@gmail.com>
2018-10-09 20:35:07 -0400

    Adds list filtering

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 import * as BreweryAPI from './api/BreweryAPI.js';
 import Header from './components/Header.js';
 import BeerList from './components/BeerList.js';  
-import { buildInfoContent } from './util/helpers.js'
 
 
 class neighborhoodMap extends Component {
 
   state = {
     breweries: [],
+    mapNJ: {},
     markers: []
   }
-
   renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBCaNfq4-xCMmvc-H8GARJxFlEJGJpyqsY&callback=initMap")
     window.initMap = this.initMap
   }
-
-
   initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 39.514327, lng: -74.663288},
@@ -26,23 +23,41 @@ class neighborhoodMap extends Component {
       mapTypeControl: false,
       fullscreenControl: false
     })
-    this.state.breweries.map(brewery => {
+    this.setState({mapNJ: map})
+    window.google.maps.event.addListenerOnce(map, 'idle', () => {
+          this.setState({mapNJ: map})
+          this.makeMarkers(this.state.breweries)
+    });
+  }
+
+  makeMarkers = (locations) => {
+    let markerArray = []
+    locations.map(brewery => {
       let marker = new window.google.maps.Marker({
         position: {lat: brewery.location.lat, lng: brewery.location.lng},
-        map: map,
+        map: this.state.mapNJ,
         title: brewery.name,
         animation: window.google.maps.Animation.DROP,
       })
-      console.log(brewery.name, brewery.location.address)
       let infowindow =  new window.google.maps.InfoWindow({
         content: brewery.categories[0].shortName
       })
       marker.addListener('click', function() {
-        infowindow.open(map, marker);
+        infowindow.open(this.state.mapNJ, marker);
       });
+      markerArray.push(marker);
     })
-
+    console.log(markerArray)
+    this.setState({ markers: markerArray})
   }
+  
+hideMarkers = (markersArray) => {
+  for (let i = 0; i < markersArray.length; i++) {
+    markersArray[i].visible = false;
+  }
+  this.setState({ markers: markersArray })
+}
+
 
   componentDidMount() {
     BreweryAPI.getAll()
@@ -51,15 +66,19 @@ class neighborhoodMap extends Component {
     });
   }
 
+  componentDidUpdate() {
+  console.log(typeof(this.state.markers))
+}
+
   render() {
     return (
       <div>
-        <Header />
         <div className='container'>
-        <BeerList breweries={this.state.breweries}/>
-        <div className='map-container'>
-          <div id='map'></div>
-        </div>
+          <Header />
+          <BeerList breweries={this.state.breweries} markers={this.state.markers}/>
+          <div className='map-container'>
+            <div id='map'></div>
+          </div>
         </div>
       </div>
     )
--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 import '../App.css';
-import * as BreweryAPI from '../api/BreweryAPI.js';
+//import * as BreweryAPI from '../api/BreweryAPI.js';
 import PropTypes from 'prop-types';
+import escapeRegEx from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class BeerList extends Component {
 	static propTypes = {
 		breweries: PropTypes.array.isRequired,
+		addMarkers: PropTypes.func.isRequired,
+		markers: PropTypes.array.isRequired,
+		hideMarkers: PropTypes.func.isRequired,
+
+		// mapNJ: PropTypes.object.isRequired
 	}
-componentDidMount() {
+state = {
+	query: '',
+}
+
+updateQuery = (query) => {
+	this.setState({ query: query.trim() })
+}
+
+clearQuery = () => {
+	this.setState({query: '' })
+}
+
+
+
+componentDidUpdate() {
+
 };
 	render () {
-			console.log(this.props.breweries)
+		const { breweries, mapNJ, markers, hideMarkers } = this.props
+		const { query } = this.state
+		let filteredList
+		if (this.state.query) {
+			const match = new RegExp(escapeRegEx(this.state.query), 'i')
+			filteredList = breweries.filter((brewery) => match.test(brewery.location.city))
+		} else {
+			filteredList = breweries
+		}
+
+		filteredList.sort(sortBy('name'))
 		return (
 			<div className='beer-list-container'>
 				<div className='filter-brewery'>
@@ -19,16 +51,22 @@ componentDidMount() {
 						placeholder='filter by city' 
 						className='query'
 						role='search'
+						value={query}
+						onChange={event => {
+							this.updateQuery(event.target.value)
+							props.hideMarkers(markers)
+
+						}}
 					/>
 				</div>
 				<ul className='beer-list'>
-					{this.props.breweries.map((brewery) => (
-					<li key={brewery.id} className='contact-list-item'>
-						<div className='contact-details'>
-							<p>{brewery.name}</p>
-							<p>{brewery.location.city}</p>
-						</div>
-					</li>
+					{filteredList.map((brewery) => (
+						<li key={brewery.id} className='contact-list-item'>
+							<div className='contact-details'>
+								<p>{brewery.name}</p>
+								<p>{brewery.location.city}</p>
+							</div>
+						</li>
 					))}
 				</ul>
 			</div>
