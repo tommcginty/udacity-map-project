74219fecb0658dc190fe04af2c1953ec49a5ead5
Tom McGinty <thmcginty@gmail.com>
2018-11-04 00:43:12 -0400

    Fixes gitignore

--- src/credentials/Credentials.js
+++
@@ -1,6 +0,0 @@
-//  Foursquare API keys:
-export const ClientID = "OLSC5NDYPPZVPXAS3KM0NTJ5404JN0MICQ4F2YLBF00KYV1H";
-export const ClientSecret = "4HUJIISYVN1PZPOBDO1P3FAGEFA5TEK0GGUI02HRAE3U0FZ0";
-
-//   Google Maps API key
-export const MapKey = "AIzaSyBCaNfq4-xCMmvc-H8GARJxFlEJGJpyqsY";
