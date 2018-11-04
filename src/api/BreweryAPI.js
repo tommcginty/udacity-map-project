7b9c22985d5dcdf7a8fc5821b1a8ca1f110232ff
Tom McGinty <thmcginty@gmail.com>
2018-11-03 22:06:32 -0400

    Removed some commented-out code

--- src/api/BreweryAPI.js
+++ src/api/BreweryAPI.js
@@ -1,9 +1,6 @@
-// const api = "https://api.foursquare.com/v2/venues/search?ll=39.3643,-74.4229&radius=70000&categoryId=50327c8591d4c4b30a586d5d&client_id=OLSC5NDYPPZVPXAS3KM0NTJ5404JN0MICQ4F2YLBF00KYV1H&client_secret=4HUJIISYVN1PZPOBDO1P3FAGEFA5TEK0GGUI02HRAE3U0FZ0&v=20181005"
 import { ClientID, ClientSecret } from "../credentials/Credentials.js"
 
 const fsURL = "https://api.foursquare.com/v2/venues/",
-	  //clientID = "OLSC5NDYPPZVPXAS3KM0NTJ5404JN0MICQ4F2YLBF00KYV1H",
-	  // clientSecret = "4HUJIISYVN1PZPOBDO1P3FAGEFA5TEK0GGUI02HRAE3U0FZ0",
 	  version = "20181005",
 	  search = "search?ll=39.3643,-74.4229&radius=70000&categoryId=50327c8591d4c4b30a586d5d"
  // Gets all breweies and filter ones that actually have an address and who are actually breweries
