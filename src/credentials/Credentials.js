41383c696f21d71c5018761fdfa08e44bbb468c0
Tom McGinty <thmcginty@gmail.com>
2018-11-04 11:51:13 -0500

    Adds redacted API Keys

--- src/credentials/Credentials.js
+++ src/credentials/Credentials.js
@@ -3,4 +3,4 @@ export const ClientID = <YOUR FOURSQUARE CLIENT ID>;
 export const ClientSecret = <YOUR FOURSQUARE CLIENT SECRET>
 
 //   Google Maps API key
-export const MapKey = <YOUR GOOGLE API>;
+export const MapKey = <YOUR GOOGLE API KEY>;
