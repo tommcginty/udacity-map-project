96c86c3fa861f202e48c58fceac38da871138934
Tom McGinty <thmcginty@gmail.com>
2018-11-04 11:48:17 -0500

    Adds redacted api keys and client secrest

---
+++ src/credentials/Credentials.js
@@ -0,0 +1,6 @@
+//  Foursquare API keys:
+export const ClientID = <YOUR FOURSQUARE CLIENT ID>;
+export const ClientSecret = <YOUR FOURSQUARE CLIENT SECRET>
+
+//   Google Maps API key
+export const MapKey = <YOUR GOOGLE API>;
